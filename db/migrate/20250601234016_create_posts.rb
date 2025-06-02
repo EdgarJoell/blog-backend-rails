1d1dc6ea50d91a6ee509904fdca8d4888b33ec78
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-02 18:43:16 -0400

    Update migration file for Posts table.

--- db/migrate/20250601234016_create_posts.rb
+++ db/migrate/20250601234016_create_posts.rb
@@ -1,8 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.string :author
+    create_table :posts, force: :cascade do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :content
+      t.boolean :published, default: false, null: false
+      t.boolean :is_latest, default: true, null: false
 
       t.timestamps
     end
