0415364d4665a9d7b63f57d9e68870004bef4e0f
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-02 19:24:00 -0400

    Update schema and migration files.

---
+++ db/migrate/20250602224900_create_posts_table.rb
@@ -0,0 +1,13 @@
+class CreatePostsTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts, force: :cascade do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :content
+      t.boolean :published, default: false, null: false
+      t.boolean :is_latest, default: true, null: false
+
+      t.timestamps
+    end
+  end
+end
