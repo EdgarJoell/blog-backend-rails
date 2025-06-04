b89fedd190460e22164fac05b5400a1310979b8d
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-03 22:45:02 -0400

    Add endpoint functionality for Authors.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,57 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+    render json: @authors, status: 200
+  end
+
+  def show
+    begin
+      @author = Author.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "No record with this ID was found." }, status: :not_found
+    end
+
+    render json: @author, status: 200
+  end
+
+  def create
+    @author = Author.new(post_params)
+
+    unless @author.save
+      render json: @author.errors, status: :unprocessable_content
+    end
+
+    render json: @author, status: 201
+  end
+
+  def update
+    begin
+      @author = Author.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "There was no record found with this ID" }, status: 404
+    end
+
+    unless @author.update(post_params)
+      render json: @author.errors, status: :unprocessable_entity
+    end
+
+    render json: @author, status: 200
+  end
+
+  def destroy
+    begin
+      @author = Author.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "There was no record found with this ID" }, status: 404
+    end
+
+    unless @author.destroy
+      render json: { message: "Could not delete this record. Please try again." }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:author).permit(:first_name, :last_name, :email)
+  end
+end
