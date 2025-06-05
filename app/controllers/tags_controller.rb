5248d1be3d8de788796cbd78ba340e53513e17b2
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-05 14:00:05 -0400

    Create Tags controller and test file. Add API methods for Tags.

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,53 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+    render json: @tags, status: :ok
+  end
+
+  def show
+    @tag = Tag.find_by(id: params[:id])
+
+    return render json: { error: "Tag not found." }, status: :not_found if @tag.nil?
+
+    render json: @tag, status: :ok
+  end
+
+  def create
+    @tag = Tag.new(post_params)
+
+    if @tag.save
+      render json: @tag, status: :created
+    end
+
+    render json: { error: "Tag could not be created." }, status: :bad_request
+  end
+
+  def update
+    @tag = Tag.find_by(id: params[:id])
+
+    return render json: { error: "Tag not found." }, status: :not_found if @tag.nil?
+
+    if @tag.update(post_params)
+      render json: @tag, status: :ok
+    end
+
+    render json: { error: "Tag could not be updated." }, status: :bad_request
+  end
+
+  def destroy
+    @tag = Tag.find_by(id: params[:id])
+
+    return render json: { error: "Tag not found." }, status: :not_found if @tag.nil?
+
+    if @tag.destroy
+      head :ok
+    end
+
+    render json: { error: "Tag could not be destroyed." }, status: :internal_server_error
+  end
+
+  private
+  def post_params
+    params.require(:tag).permit(:name, :color)
+  end
+end
